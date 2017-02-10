1e715186a529e891e40a63e6d0525d2a93457a2a
Миша Миллэр <miller2@borat.local>
2017-02-09 23:38:01 -0800

    fixed abhorrent styling choices

--- arrisstats/cli.py
+++ arrisstats/cli.py
@@ -11,13 +11,13 @@ class Cli:
 
     def process_stream_table(self, stream_data_table):
         stream_data = {}
-        headers = [ td.text for td in stream_data_table[0].findAll("td") ]
+        headers = [td.text for td in stream_data_table[0].findAll("td")]
         for tr in stream_data_table[1:]:
-            data_row = [ td.text for td in tr.findAll("td")]
+            data_row = [td.text for td in tr.findAll("td")]
             data_stream = {}
-            for i in range(1,len(headers)):
+            for i in range(1, len(headers)):
                 data_stream[headers[i]] = data_row[i]
-            if self.debug: print(data_stream)
+            if self.debug: print data_stream
             stream_data[data_row[0]] = data_stream
         return stream_data
 
@@ -35,7 +35,7 @@ class Cli:
                     status["upstream"] = self.process_stream_table(h4.findNext("table").findAll("tr"))
             #print(json.dumps(status["downstream"]))
             #print(json.dumps(status["upstream"]))
-            print(json.dumps(status))
+            print json.dumps(status)
 
 
 if __name__ == "__main__":
