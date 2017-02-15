9706de35656b8c5c484dd5dddde16937657d5a49
David Lovitch <dlovitch@gmail.com>
2017-02-15 14:42:10 -0800

    Housekeeping

---
+++ setup.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+
+requires = [
+    'beautifulsoup4>=4.5.3',
+    'click>=6.7',
+    'lxml>=3.7.2',
+    'requests>=2.13.0',
+    ]
+
+setup(
+    name='arrisstats',
+    version='1.0.0',
+    description='Stats from Arris cable modems',
+    author='David Lovitch',
+    author_email='dlovitch@gmail.com',
+    url='https://github.com/dlovitch/arrisstats/',
+    packages=find_packages(),
+    install_requires=requires
+    )
